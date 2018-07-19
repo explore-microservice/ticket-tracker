d46624318aed466bf84953329e09b890ccd492df
Berko Szabolcs <szberko@gmail.com>
2018-07-19 13:31:18 +0200

    remove unnecessary constructor

--- src/main/java/com/issuetracker/webapp/service/MessageProviderServiceImpl.java
+++ src/main/java/com/issuetracker/webapp/service/MessageProviderServiceImpl.java
@@ -7,9 +7,6 @@ public class MessageProviderServiceImpl implements MessageProviderService {
 
     private static final String template = "Hello, %s!";
 
-    public MessageProviderServiceImpl() {
-    }
-
     @Override
     public String getMessage(String name) {
         return String.format(template, name);
