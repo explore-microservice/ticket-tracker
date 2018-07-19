180531c1388d35ca16ed21ace98c386f33d9dc7d
Berko Szabolcs <szberko@gmail.com>
2018-07-19 09:04:53 +0200

    remove id from MessageProvider

--- src/main/java/com/issuetracker/webapp/Greeting.java
+++ src/main/java/com/issuetracker/webapp/Greeting.java
@@ -2,17 +2,11 @@ package com.issuetracker.webapp;
 
 public class Greeting {
 
-    private final long id;
     private final String content;
 
 
-    public Greeting(long id, String content) {
-        this.id = id;
+    public Greeting(String content) {
         this.content = content;
-    }
-
-    public long getId() {
-        return id;
     }
 
     public String getContent() {
