ba802711d966f93b380197166361732bce5fd05a
Berko Szabolcs <szberko@gmail.com>
2018-07-19 09:31:30 +0200

    removing unnecessary Greeting class

--- src/main/java/com/issuetracker/webapp/GreetingController.java
+++ src/main/java/com/issuetracker/webapp/GreetingController.java
@@ -4,15 +4,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.concurrent.atomic.AtomicLong;
-
 @RestController
 public class GreetingController{
 
     private static final String template = "Hello, %s!";
 
     @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name){
-        return new Greeting(String.format(template, name));
+    public String greeting(@RequestParam(value = "name", defaultValue = "World") String name){
+        return String.format(template, name);
     }
 }
