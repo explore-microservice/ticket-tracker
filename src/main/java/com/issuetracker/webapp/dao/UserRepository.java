f4f440d3febeaeaa5200508c85b650ccacdb1320
Berko Szabolcs <szberko@gmail.com>
2018-07-23 17:07:27 +0200

    implement UserRepository DAO class

--- src/main/java/com/issuetracker/webapp/dao/UserRepository.java
+++ src/main/java/com/issuetracker/webapp/dao/UserRepository.java
@@ -1,9 +1,20 @@
 package com.issuetracker.webapp.dao;
 
 import com.issuetracker.webapp.pojo.User;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 
-public interface UserRepository extends CrudRepository<User, Long> {
+@Repository
+public class UserRepository {
 
+    private JdbcTemplate jdbcTemplate;
 
+    public UserRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public User findById(Long id){
+        return jdbcTemplate.queryForObject("SELECT * FROM it_user WHERE id=?;", new Object[] { id }, new BeanPropertyRowMapper<>(User.class));
+    }
 }
