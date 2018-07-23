0d23ce215bb318434cbc13166da38e8252467dd4
Berko Szabolcs <szberko@gmail.com>
2018-07-23 17:41:23 +0200

    implementing User service class and interface

---
+++ src/main/java/com/issuetracker/webapp/service/UserService.java
@@ -0,0 +1,10 @@
+package com.issuetracker.webapp.service;
+
+import com.issuetracker.webapp.pojo.User;
+
+import java.util.List;
+
+public interface UserService {
+
+    List<User> getAllUsers();
+}
---
+++ src/main/java/com/issuetracker/webapp/service/UserServiceImpl.java
@@ -0,0 +1,22 @@
+package com.issuetracker.webapp.service;
+
+import com.issuetracker.webapp.dao.UserRepository;
+import com.issuetracker.webapp.pojo.User;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+}
