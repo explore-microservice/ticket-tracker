eec295abedefd30ef0d629e7f51c95ca792a86d5
Berko Szabolcs <szberko@gmail.com>
2018-07-24 12:54:47 +0200

    annotate mandatory fields with @NotNull annotation

--- src/main/java/com/issuetracker/webapp/pojo/User.java
+++ src/main/java/com/issuetracker/webapp/pojo/User.java
@@ -1,24 +1,28 @@
 package com.issuetracker.webapp.pojo;
 
+import lombok.Data;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 @Entity
+@Data
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-    private String firstname;
-    private String lastname;
-    private String username;
-    private String password;
+    private @NotNull Long id;
+    private @NotNull String firstname;
+    private @NotNull String lastname;
+    private @NotNull String username;
+    private @NotNull String password;
     private LocalDateTime lastLoggedIn;
 
-    protected User() { }
+    public User() { }
 
     public User(String firstname, String lastname, String username, String password, LocalDateTime lastLoggedIn) {
         this.firstname = firstname;
@@ -26,63 +30,5 @@ public class User {
         this.username = username;
         this.password = password;
         this.lastLoggedIn = lastLoggedIn;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getFirstname() {
-        return firstname;
-    }
-
-    public void setFirstname(String firstname) {
-        this.firstname = firstname;
-    }
-
-    public String getLastname() {
-        return lastname;
-    }
-
-    public void setLastname(String lastname) {
-        this.lastname = lastname;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public LocalDateTime getLastLoggedIn() {
-        return lastLoggedIn;
-    }
-
-    public void setLastLoggedIn(LocalDateTime lastLoggedIn) {
-        this.lastLoggedIn = lastLoggedIn;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", firstname='" + firstname + '\'' +
-                ", lastname='" + lastname + '\'' +
-                ", username='" + username + '\'' +
-                '}';
     }
 }
