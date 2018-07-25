4f3f6414ac65d7c36c2b6cb18f0925cdf944ffcf
Berko Szabolcs <szberko@gmail.com>
2018-07-25 13:09:20 +0200

    implement findById test case with both TestNG and Junit for testing purposes

--- src/test/java/com/issuetracker/webapp/dao/UserRepositoryTest.java
+++ src/test/java/com/issuetracker/webapp/dao/UserRepositoryTest.java
@@ -1,8 +1,10 @@
 package com.issuetracker.webapp.dao;
 
+import com.issuetracker.webapp.pojo.User;
 import com.issuetracker.webapp.pojo.UserRowMapper;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -14,23 +16,42 @@ import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.*;
 import static org.mockito.Mockito.*;
 
+
 public class UserRepositoryTest {
 
     @Mock
-    private JdbcTemplate jdbcTemplate;
+    private JdbcTemplate jdbcTemplateMock;
 
     private UserRepository userRepository;
 
     @BeforeMethod
     public void init() {
         MockitoAnnotations.initMocks(this);
-        userRepository = new UserRepository(jdbcTemplate);
+        userRepository = new UserRepository(jdbcTemplateMock);
     }
 
     @Test
-    public void testShouldReturnEmptyListWhenJDBCTemplateReturnsEmptyList() {
-        when(jdbcTemplate.query("SELECT * FROM it_user;", new UserRowMapper())).thenReturn(new ArrayList<>());
-        assertThat("UserRepository should return empty list when jdbcTemplate returns empty list.",
+    public void testFindAllShouldReturnEmptyListWhenJDBCTemplateReturnsEmptyList() {
+        when(jdbcTemplateMock.query(UserRepository.findAllQuery, new UserRowMapper())).thenReturn(new ArrayList<>());
+        assertThat("UserRepository should return empty list when jdbcTemplateMock returns empty list.",
                 Collections.EMPTY_LIST, equalTo(userRepository.findAll()));
+    }
+
+
+    @Test
+    public void testFindByIdShouldReturnAnEntryWithTheIdIfJDBCTemplateReturnsAnEntry(){
+        User expectedUser = User.builder()
+                .id(1L)
+                .firstname("firstname")
+                .lastname("lastname")
+                .username("username")
+                .password("password")
+                .build();
+        when(jdbcTemplateMock.queryForObject(UserRepository.findById, new Object[] { 1L }, new BeanPropertyRowMapper<>(User.class)))
+                .thenReturn(expectedUser);
+
+        User actualUser = userRepository.findById(1L);
+
+        assertThat(actualUser, equalTo(expectedUser));
     }
 }
