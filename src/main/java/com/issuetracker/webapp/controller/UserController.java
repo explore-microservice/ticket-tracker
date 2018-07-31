28f12dbedc09eb08854371a5461321c42aaff222
Berko Szabolcs <szberko@gmail.com>
2018-07-31 13:46:06 +0200

    remove .* from import in UserController

--- src/main/java/com/issuetracker/webapp/controller/UserController.java
+++ src/main/java/com/issuetracker/webapp/controller/UserController.java
@@ -2,7 +2,10 @@ package com.issuetracker.webapp.controller;
 
 import com.issuetracker.webapp.pojo.User;
 import com.issuetracker.webapp.service.UserService;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
