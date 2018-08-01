102ea1c37a45089107723a527b94677592036758
Berko Szabolcs <szberko@gmail.com>
2018-08-01 14:58:42 +0200

    remove JPA annotations

--- src/main/java/com/issuetracker/webapp/pojo/Project.java
+++ src/main/java/com/issuetracker/webapp/pojo/Project.java
@@ -3,33 +3,17 @@ package com.issuetracker.webapp.pojo;
 import lombok.Builder;
 import lombok.Data;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
-import java.util.Set;
 
 @Data
 @Builder
-@Entity
-@Table(name = "it_project")
 public class Project {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
     private @NotNull Long id;
     private @NotNull String name;
     private String description;
     private @NotNull LocalDateTime creationdate;
     private LocalDateTime startdate;
     private LocalDateTime enddate;
-
-    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER, mappedBy = "user")
-    private Set<UserWorkOnProject> users;
 }
--- src/main/java/com/issuetracker/webapp/pojo/User.java
+++ src/main/java/com/issuetracker/webapp/pojo/User.java
@@ -4,26 +4,13 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NonNull;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
-import java.util.Set;
 
 @Data
 @Builder
-@Entity
-@Table(name = "it_user")
 public class User {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
     private @NonNull Long id;
     private @NonNull String firstname;
     private @NonNull String lastname;
@@ -31,7 +18,4 @@ public class User {
     private @NotNull String email;
     private @NonNull String password;
     private LocalDateTime lastloggedin;
-
-    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER, mappedBy = "project")
-    private Set<UserWorkOnProject> projects;
 }
