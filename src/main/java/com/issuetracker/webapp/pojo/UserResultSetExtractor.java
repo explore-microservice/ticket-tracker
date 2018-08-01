6850d32a32f212a12eca82bfea2642bb3022d0e9
Berko Szabolcs <szberko@gmail.com>
2018-08-01 15:12:34 +0200

    implement custom result set extractor for UserWithProjects POJO class

---
+++ src/main/java/com/issuetracker/webapp/pojo/UserResultSetExtractor.java
@@ -0,0 +1,53 @@
+package com.issuetracker.webapp.pojo;
+
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.ResultSetExtractor;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Optional;
+
+public class UserResultSetExtractor implements ResultSetExtractor<Collection<UserWithProjects>> {
+
+    @Override
+    public Collection<UserWithProjects> extractData(ResultSet rs) throws SQLException, DataAccessException {
+        HashMap<Long, UserWithProjects> users = new HashMap<>();
+
+        while(rs.next()){
+            Long userKey = rs.getLong("userid");
+
+            if(!users.containsKey(userKey)){
+                users.put(userKey, UserWithProjects.builder()
+                        .user(User.builder()
+                                .id(userKey)
+                                .firstname(rs.getString("firstname"))
+                                .lastname(rs.getString("lastname"))
+                                .username(rs.getString("username"))
+                                .email(rs.getString("email"))
+                                .password(rs.getString("password"))
+                                .lastloggedin(Optional.ofNullable(rs.getTimestamp("lastloggedin")).map(Timestamp::toLocalDateTime).orElse(null))
+                                .build())
+                        .projects(new HashSet<>())
+                .build());
+            }
+
+            users.get(userKey).addProject(
+                    Project.builder()
+                            .id(rs.getLong("projectid"))
+                            .name(rs.getString("name"))
+                            .description(rs.getString("description"))
+                            .creationdate(rs.getTimestamp("creationdate").toLocalDateTime())
+                            .startdate(Optional.ofNullable(rs.getTimestamp("startdate"))
+                                    .map(Timestamp::toLocalDateTime).orElse(null))
+                            .enddate(Optional.ofNullable(rs.getTimestamp("enddate"))
+                                    .map(Timestamp::toLocalDateTime).orElse(null))
+                            .build()
+            );
+        }
+        return users.values();
+    }
+}
