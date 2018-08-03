4279bd92a3a505fc409d6c089c8f13ba0e908717
Berko Szabolcs <szberko@gmail.com>
2018-08-03 13:32:07 +0200

    implementing unit tests against UserRepository.findAllUsersOnAProject()

--- src/test/java/com/issuetracker/webapp/dao/UserRepositoryTest.java
+++ src/test/java/com/issuetracker/webapp/dao/UserRepositoryTest.java
@@ -1,5 +1,6 @@
 package com.issuetracker.webapp.dao;
 
+import com.issuetracker.webapp.exceptions.ProjectNotFoundException;
 import com.issuetracker.webapp.pojo.User;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
@@ -9,13 +10,16 @@ import org.testng.annotations.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
 import static com.issuetracker.webapp.dao.UserRepository.FIND_ALL_QUERY;
+import static com.issuetracker.webapp.dao.UserRepository.FIND_ALL_USERS_WHO_WORKS_ON_PROJECT;
 import static com.issuetracker.webapp.dao.UserRepository.FIND_USER_BY_ID_QUERY;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 
 public class UserRepositoryTest {
@@ -27,6 +31,7 @@ public class UserRepositoryTest {
     private static final Long EXPECTED_USER_ID = 1L;
     private static final User EXPECTED_USER = aUser(EXPECTED_USER_ID);
     private static final List<User> EXPECTED_LIST_OF_USERS = aListOfUsers();
+    private static final Long EXPECTED_PROJECT_ID = 1L;
 
     @Mock
     private JdbcTemplate jdbcTemplate;
@@ -63,6 +68,25 @@ public class UserRepositoryTest {
         when(jdbcTemplate.queryForObject(FIND_USER_BY_ID_QUERY, new Object[] { EXPECTED_USER_ID }, userRepository.userRowMapper)).thenReturn(null);
         User actualUser = userRepository.findById(EXPECTED_USER_ID);
         assertThat(actualUser, equalTo(null));
+    }
+
+    @Test
+    public void shouldReturnAllUsersWhoWorksOnProjectWhenThereAreEntriesInTheDB(){
+        when(jdbcTemplate.query(FIND_ALL_USERS_WHO_WORKS_ON_PROJECT, new Object[] { EXPECTED_PROJECT_ID }, userRepository.userRowMapper)).thenReturn(EXPECTED_LIST_OF_USERS);
+        List<User> actualUsersWhoWorkOnProject = userRepository.findAllUsersOnAProject(EXPECTED_PROJECT_ID);
+        assertThat(actualUsersWhoWorkOnProject, equalTo(EXPECTED_LIST_OF_USERS));
+    }
+
+    @Test(expectedExceptions = ProjectNotFoundException.class)
+    public void shouldThrowExceptionWhenThereIsNoProjectInTheDBWithTheRequestedId(){
+        when(jdbcTemplate.query(FIND_ALL_USERS_WHO_WORKS_ON_PROJECT, new Object[] { EXPECTED_PROJECT_ID }, userRepository.userRowMapper)).thenThrow(ProjectNotFoundException.class);
+        userRepository.findAllUsersOnAProject(EXPECTED_PROJECT_ID);
+    }
+
+    @Test
+    public void shouldReturnEmptyListOfUsersWhenThereIsNoOneWorkingOnARequestedProject(){
+        when(jdbcTemplate.query(FIND_ALL_USERS_WHO_WORKS_ON_PROJECT, new Object[] { EXPECTED_PROJECT_ID }, userRepository.userRowMapper)).thenReturn(new ArrayList<>());
+        assertThat(userRepository.findAllUsersOnAProject(EXPECTED_PROJECT_ID), equalTo(Collections.EMPTY_LIST));
     }
 
     private static User aUser(Long id) {
