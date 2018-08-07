28de96684076d153126666f48f24b90cf2f7d67d
Berko Szabolcs <szberko@gmail.com>
2018-08-07 13:10:17 +0200

    introducing ColumnUtils which holds all the column names for different entities

---
+++ src/main/java/com/issuetracker/webapp/pojo/utils/ColumnUtils.java
@@ -0,0 +1,21 @@
+package com.issuetracker.webapp.pojo.utils;
+
+public abstract class ColumnUtils {
+
+    public static final String USER_PREFIX = "user";
+    public static final String USER_ID = "id";
+    public static final String USER_FIRSTNAME = "firstname";
+    public static final String USER_LASTNAME = "lastname";
+    public static final String USER_USERNAME = "username";
+    public static final String USER_EMAIL = "email";
+    public static final String USER_PASSWORD = "password";
+    public static final String USER_LASTLOGGEDIN = "lastloggedin";
+
+    public static final String PROJECT_PREFIX = "project";
+    public static final String PROJECT_ID = "id";
+    public static final String PROJECT_NAME = "name";
+    public static final String PROJECT_DESCRIPTION = "description";
+    public static final String PROJECT_CREATIONDATE = "creationdate";
+    public static final String PROJECT_STARTDATE = "startdate";
+    public static final String PROJECT_ENDDATE = "enddate";
+}
