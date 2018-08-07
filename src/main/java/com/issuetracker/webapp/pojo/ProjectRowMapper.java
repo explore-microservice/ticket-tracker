c96e31544ef7b580b7d1090f9e6c35aac1771c7a
Berko Szabolcs <szberko@gmail.com>
2018-08-07 13:10:42 +0200

    make a use of the new ColumnUtils class in all of the mappers.

--- src/main/java/com/issuetracker/webapp/pojo/ProjectRowMapper.java
+++ src/main/java/com/issuetracker/webapp/pojo/ProjectRowMapper.java
@@ -7,18 +7,25 @@ import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.util.Optional;
 
+import static com.issuetracker.webapp.pojo.utils.ColumnUtils.PROJECT_CREATIONDATE;
+import static com.issuetracker.webapp.pojo.utils.ColumnUtils.PROJECT_DESCRIPTION;
+import static com.issuetracker.webapp.pojo.utils.ColumnUtils.PROJECT_ENDDATE;
+import static com.issuetracker.webapp.pojo.utils.ColumnUtils.PROJECT_ID;
+import static com.issuetracker.webapp.pojo.utils.ColumnUtils.PROJECT_NAME;
+import static com.issuetracker.webapp.pojo.utils.ColumnUtils.PROJECT_STARTDATE;
+
 public class ProjectRowMapper implements RowMapper<Project> {
 
     @Override
     public Project mapRow(ResultSet resultSet, int rowNum) throws SQLException {
         return Project.builder()
-                .id(resultSet.getLong("id"))
-                .name(resultSet.getString("name"))
-                .description(resultSet.getString("description"))
-                .creationDate(resultSet.getTimestamp("creationDate").toLocalDateTime())
-                .startDate(Optional.ofNullable(resultSet.getTimestamp("startDate"))
+                .id(resultSet.getLong(PROJECT_ID))
+                .name(resultSet.getString(PROJECT_NAME))
+                .description(resultSet.getString(PROJECT_DESCRIPTION))
+                .creationDate(resultSet.getTimestamp(PROJECT_CREATIONDATE).toLocalDateTime())
+                .startDate(Optional.ofNullable(resultSet.getTimestamp(PROJECT_STARTDATE))
                         .map(Timestamp::toLocalDateTime).orElse(null))
-                .endDate(Optional.ofNullable(resultSet.getTimestamp("endDate"))
+                .endDate(Optional.ofNullable(resultSet.getTimestamp(PROJECT_ENDDATE))
                         .map(Timestamp::toLocalDateTime).orElse(null))
                 .build();
     }
--- src/main/java/com/issuetracker/webapp/pojo/UserResultSetExtractor.java
+++ src/main/java/com/issuetracker/webapp/pojo/UserResultSetExtractor.java
@@ -1,5 +1,6 @@
 package com.issuetracker.webapp.pojo;
 
+import com.issuetracker.webapp.pojo.utils.ColumnUtils;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.ResultSetExtractor;
 
@@ -10,6 +11,22 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.Optional;
 
+import static com.issuetracker.webapp.pojo.utils.ColumnUtils.PROJECT_CREATIONDATE;
+import static com.issuetracker.webapp.pojo.utils.ColumnUtils.PROJECT_DESCRIPTION;
+import static com.issuetracker.webapp.pojo.utils.ColumnUtils.PROJECT_ENDDATE;
+import static com.issuetracker.webapp.pojo.utils.ColumnUtils.PROJECT_ID;
+import static com.issuetracker.webapp.pojo.utils.ColumnUtils.PROJECT_NAME;
+import static com.issuetracker.webapp.pojo.utils.ColumnUtils.PROJECT_PREFIX;
+import static com.issuetracker.webapp.pojo.utils.ColumnUtils.PROJECT_STARTDATE;
+import static com.issuetracker.webapp.pojo.utils.ColumnUtils.USER_EMAIL;
+import static com.issuetracker.webapp.pojo.utils.ColumnUtils.USER_FIRSTNAME;
+import static com.issuetracker.webapp.pojo.utils.ColumnUtils.USER_ID;
+import static com.issuetracker.webapp.pojo.utils.ColumnUtils.USER_LASTLOGGEDIN;
+import static com.issuetracker.webapp.pojo.utils.ColumnUtils.USER_LASTNAME;
+import static com.issuetracker.webapp.pojo.utils.ColumnUtils.USER_PASSWORD;
+import static com.issuetracker.webapp.pojo.utils.ColumnUtils.USER_PREFIX;
+import static com.issuetracker.webapp.pojo.utils.ColumnUtils.USER_USERNAME;
+
 public class UserResultSetExtractor implements ResultSetExtractor<Collection<UserWithProjects>> {
 
     @Override
@@ -17,29 +34,29 @@ public class UserResultSetExtractor implements ResultSetExtractor<Collection<Use
         HashMap<Long, UserWithProjects> usersWithRelatedProjects = new HashMap<>();
 
         while(rs.next()){
-            Long userKey = rs.getLong("userid");
+            Long userKey = rs.getLong(USER_PREFIX + USER_ID);
 
             usersWithRelatedProjects.putIfAbsent(userKey, UserWithProjects.builder()
                     .user(User.builder()
                             .id(userKey)
-                            .firstName(rs.getString("firstName"))
-                            .lastName(rs.getString("lastName"))
-                            .username(rs.getString("username"))
-                            .email(rs.getString("email"))
-                            .password(rs.getString("password"))
-                            .lastLoggedIn(Optional.ofNullable(rs.getTimestamp("lastLoggedIn"))
+                            .firstName(rs.getString(USER_FIRSTNAME))
+                            .lastName(rs.getString(USER_LASTNAME))
+                            .username(rs.getString(USER_USERNAME))
+                            .email(rs.getString(USER_EMAIL))
+                            .password(rs.getString(USER_PASSWORD))
+                            .lastLoggedIn(Optional.ofNullable(rs.getTimestamp(USER_LASTLOGGEDIN))
                                     .map(Timestamp::toLocalDateTime).orElse(null))
                             .build())
             .build());
 
             Project project = Project.builder()
-                    .id(rs.getLong("projectid"))
-                    .name(rs.getString("name"))
-                    .description(rs.getString("description"))
-                    .creationDate(rs.getTimestamp("creationDate").toLocalDateTime())
-                    .startDate(Optional.ofNullable(rs.getTimestamp("startDate"))
+                    .id(rs.getLong(PROJECT_PREFIX + PROJECT_ID))
+                    .name(rs.getString(PROJECT_NAME))
+                    .description(rs.getString(PROJECT_DESCRIPTION))
+                    .creationDate(rs.getTimestamp(PROJECT_CREATIONDATE).toLocalDateTime())
+                    .startDate(Optional.ofNullable(rs.getTimestamp(PROJECT_STARTDATE))
                             .map(Timestamp::toLocalDateTime).orElse(null))
-                    .endDate(Optional.ofNullable(rs.getTimestamp("endDate"))
+                    .endDate(Optional.ofNullable(rs.getTimestamp(PROJECT_ENDDATE))
                             .map(Timestamp::toLocalDateTime).orElse(null))
                     .build();
 
