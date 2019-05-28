0d0f97fcb87021df03f3694940585754c4542ef1
Szabolcs Berko <szberko@gmail.com>
2019-05-29 00:19:08 +0200

    modify data.sql and schema.sql accordingly to JPA models

--- src/test/java/com/issuetracker/webapp/schema.sql
+++ src/test/java/com/issuetracker/webapp/schema.sql
@@ -4,8 +4,8 @@ CREATE TABLE it_sprint (id bigint NOT NULL, name varchar(255) NOT NULL, descript
 CREATE TABLE it_comment (id bigint NOT NULL, content varchar(255) NOT NULL, creationdate timestamp NOT NULL, userid bigint NOT NULL, ticketid bigint NOT NULL, PRIMARY KEY (id));
 CREATE TABLE it_user (id bigint NOT NULL, firstname varchar(50) NOT NULL, lastname varchar(50) NOT NULL, username varchar(20) NOT NULL UNIQUE, email varchar(255) NOT NULL UNIQUE, password varchar(255) NOT NULL, lastloggedin timestamp, PRIMARY KEY (id));
 CREATE TABLE it_project (id bigint NOT NULL, name varchar(255) NOT NULL, description varchar(255), creationdate timestamp NOT NULL, startdate timestamp, enddate timestamp, PRIMARY KEY (id));
-ALTER TABLE it_userworksonproject ADD CONSTRAINT FKit_userwor916148 FOREIGN KEY (projectid) REFERENCES it_project (id);
-ALTER TABLE it_userworksonproject ADD CONSTRAINT FKit_userwor264313 FOREIGN KEY (userid) REFERENCES it_user (id);
+ALTER TABLE it_works ADD CONSTRAINT FKit_userwor916148 FOREIGN KEY (projectid) REFERENCES it_project (id);
+ALTER TABLE it_works ADD CONSTRAINT FKit_userwor264313 FOREIGN KEY (userid) REFERENCES it_user (id);
 ALTER TABLE it_ticket ADD CONSTRAINT creator FOREIGN KEY (creatoruserid) REFERENCES it_user (id);
 ALTER TABLE it_sprint ADD CONSTRAINT FKit_sprint113670 FOREIGN KEY (projectid) REFERENCES it_project (id);
 ALTER TABLE it_ticket ADD CONSTRAINT FKit_ticket163506 FOREIGN KEY (sprintid) REFERENCES it_sprint (id);
