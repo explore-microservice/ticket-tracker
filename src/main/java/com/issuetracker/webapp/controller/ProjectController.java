dac91e612356d1ae8240ee78951bdf94224b5558
Szabolcs Berko <szberko@gmail.com>
2019-06-01 16:21:11 +0200

    finalizing projectPage method on ProjectController level

--- src/main/java/com/issuetracker/webapp/controller/ProjectController.java
+++ src/main/java/com/issuetracker/webapp/controller/ProjectController.java
@@ -1,5 +1,6 @@
 package com.issuetracker.webapp.controller;
 
+import com.issuetracker.webapp.controller.converter.response.projectpage.ProjectResponseConverter;
 import com.issuetracker.webapp.controller.dto.response.projectpage.ProjectResponse;
 import com.issuetracker.webapp.exceptions.ProjectNotFoundException;
 import com.issuetracker.webapp.pojo.Project;
@@ -10,14 +11,17 @@ import org.springframework.web.bind.annotation.*;
 public class ProjectController {
 
     private final ProjectService projectService;
+    private final ProjectResponseConverter projectResponseConverter;
 
-    public ProjectController(final ProjectService projectService) {
+    public ProjectController(final ProjectService projectService,
+                             final ProjectResponseConverter projectResponseConverter) {
         this.projectService = projectService;
+        this.projectResponseConverter = projectResponseConverter;
     }
 
     @GetMapping(value = "/project/{id}")
     public ProjectResponse projectPage(final @PathVariable Long id) throws ProjectNotFoundException {
         final com.issuetracker.webapp.service.dto.response.projectpage.ProjectResponse projectResponse = projectService.provideProjectPage(id);
-        return null;
+        return projectResponseConverter.convert(projectResponse);
     }
 }
