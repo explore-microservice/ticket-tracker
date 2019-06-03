d924f16f7af67c18b2481b744e2e22bdab54f444
Szabolcs Berko <szberko@gmail.com>
2019-06-04 00:20:57 +0200

    add missing final from method param

--- src/main/java/com/issuetracker/webapp/controller/converter/ProjectResponseConverter.java
+++ src/main/java/com/issuetracker/webapp/controller/converter/ProjectResponseConverter.java
@@ -11,7 +11,7 @@ import java.util.stream.Collectors;
 @Component("controllerProjectResponseConverter")
 public class ProjectResponseConverter implements ControllerDTOConverter<com.issuetracker.webapp.service.dto.response.project.ProjectResponse, ProjectResponse>{
 
-    public ProjectResponse convert(com.issuetracker.webapp.service.dto.response.project.ProjectResponse projectResponse){
+    public ProjectResponse convert(final com.issuetracker.webapp.service.dto.response.project.ProjectResponse projectResponse){
         return new ProjectResponse.Builder()
                 .withName(projectResponse.getName())
                 .withDescription(projectResponse.getDescription())
