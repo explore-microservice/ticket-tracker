62bbd042b7e426e1100c389df9e9dc1b8c5fcb48
Szabolcs Berko <szberko@gmail.com>
2019-06-05 22:25:02 +0200

    user authentication. Doesn't work as it should

---
+++ src/main/java/com/issuetracker/webapp/config/SecurityConfig.java
@@ -0,0 +1,56 @@
+package com.issuetracker.webapp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+    private RestAuthenticationEntryPoint restAuthenticationEntryPoint;
+    private MySavedRequestAwareAuthenticationSuccessHandler mySuccessHandler;
+    private SimpleUrlAuthenticationFailureHandler myFailureHandler = new SimpleUrlAuthenticationFailureHandler();
+
+    public SecurityConfig(RestAuthenticationEntryPoint restAuthenticationEntryPoint,
+                          MySavedRequestAwareAuthenticationSuccessHandler mySuccessHandler){
+        this.restAuthenticationEntryPoint = restAuthenticationEntryPoint;
+        this.mySuccessHandler = mySuccessHandler;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin").password(encoder().encode("admin")).roles("ADMIN")
+                .and()
+                .withUser("user").password(encoder().encode("user")).roles("USER");
+    }
+
+    @Bean
+    public PasswordEncoder encoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .exceptionHandling()
+                .authenticationEntryPoint(restAuthenticationEntryPoint)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/projects").authenticated()
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .and()
+                .formLogin()
+                .successHandler(mySuccessHandler)
+                .failureHandler(myFailureHandler)
+                .and()
+                .logout();
+    }
+}
